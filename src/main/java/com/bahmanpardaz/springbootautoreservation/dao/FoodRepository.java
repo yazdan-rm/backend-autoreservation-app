d5e2cea9863fb9d8edcb055238c3b45bfc346920
Yazdan <1yazdanrafieemanesh@gmail.com>
2024-03-01 14:43:27 +0330

    add @CrossOrigin

--- src/main/java/com/bahmanpardaz/springbootautoreservation/dao/FoodRepository.java
+++ src/main/java/com/bahmanpardaz/springbootautoreservation/dao/FoodRepository.java
@@ -3,6 +3,8 @@ package com.bahmanpardaz.springbootautoreservation.dao;
 import com.bahmanpardaz.springbootautoreservation.entity.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface FoodRepository extends JpaRepository<Food, Long> {
 }
