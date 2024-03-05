889b66b48bdf1d13ceb1603ce1afef6f84246126
Yazdan <1yazdanrafieemanesh@gmail.com>
2024-03-06 01:18:59 +0330

    add

--- src/main/java/com/bahmanpardaz/springbootautoreservation/dao/FoodGroupRepository.java
+++ src/main/java/com/bahmanpardaz/springbootautoreservation/dao/FoodGroupRepository.java
@@ -3,7 +3,9 @@ package com.bahmanpardaz.springbootautoreservation.dao;
 import com.bahmanpardaz.springbootautoreservation.entity.FoodGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "foodGroup", path = "food-group")
 public interface FoodGroupRepository extends JpaRepository<FoodGroup, Long> {
 }
